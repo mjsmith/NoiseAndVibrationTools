4ea0bc09e7870913c905f61b2298a7bca21cc30c
Michael Smith <michael.smith@xpmt.com.au>
2017-10-05 08:56:41 +1300

    WIP

--- acoustics/ISO9613.py
+++ acoustics/ISO9613.py
@@ -8,11 +8,95 @@ class ISO9613:
         self.temp = temp
         self.humidity=humidity
     
-    def printEnvironment(self):
-        return "temp={0}, humidity={1}".format(self.temp, self.humidity)
+    def __str__(self):
+        return "ISO9613 calculator: temp={0}, humidity={1}".format(self.temp, self.humidity)
     
-    def calc(Source, Path, Receiver):
-       print("hello")
+    def calc(self, source, path, receiver):
+       
+       dist = source.location.distance(receiver.location)
+       logging.debug("Distance = {0}".format(dist))
+       
+       
+       # divergance
+       A_div = 20*np.log10(dist) - 11
+       
+       #air abs
+       A_abs = np.zeros([8,1])
+       
+       #ground abs
+       A_gr = np.zeros([8,1])
+       
+       
+       # barrier
+       A_bar = np.zeros([8,1])
+       
+       receiver.addPressure(Spectrum(source.power.linear \
+                                     + A_div + A_gr + A_abs + A_bar ))
+       
+       
+    def _isoAirAbsPerMetre(self):
+       
+       
+       
+       
+       pass
+   
+    
+    def _ISO_GroundAbsorption(dp, Gs, Gm, Gr, hs, hr):
+      
+        if dp <= 30 * (hs + hr):
+             q = 0
+        else:
+             q = 1 - ((30 * (hs + hr)) / dp) 
+     
+        A_s = np.zeros([8,1])
+        A_r = np.zeros([8,1])
+        A_m = np.zeros([8,1])
+           
+    
+    #     Case 63
+        A_s[0] = -1.5
+        A_r[0] = -1.5
+        A_m[0] = -3 * q ^ 2
+    #     Case 125
+        A_s[1] = -1.5 + Gs * ISO9613._Ground_a(hs, dp)
+        A_r[1] = -1.5 + Gr * _Ground_a(hr, dp)
+        A_m[1] = -3 * q * (1 - Gm)
+    #     Case 250
+        A_s[2] = -1.5 + Gs * _Ground_b(hs, dp)
+        A_r[2] = -1.5 + Gr * _Ground_b(hr, dp)
+        A_m[2] = -3 * q * (1 - Gm)
+    #     Case 500
+        A_s[3] = -1.5 + Gs * _Ground_c(hs, dp)
+        A_r[3] = -1.5 + Gr * _Ground_c(hr, dp)
+        A_m[3] = -3 * q * (1 - Gm)
+    #     Case 1000
+        A_s[4] = -1.5 + Gs * _Ground_d(hs, dp)
+        A_r[4] = -1.5 + Gr * _Ground_d(hr, dp)
+        A_m[4] = -3 * q * (1 - Gm)
+    #    Case 2000, 4000, 8000
+        A_s[5:7] = -1.5 * (1 - Gs)
+        A_r[5:7] = -1.5 * (1 - Gr)
+        A_m[5:7] = -3 * q * (1 - Gm)
+            
+    
+        return  A_s + A_m + A_r
+     
+    
+    
+    def _Ground_a(h, dp):
+         return 1.5 + 3 * np.exp(-0.12 * (h - 5) ^ 2) * (1 - np.exp(-dp / 50)) \
+                 + 5.7 * np.exp(-0.09 * h ^ 2) * (1 - np.exp(-2.8 * 10 ^ (-6) * dp ^ 2))
+    
+    def _Ground_b(h, dp):
+         return 1.5 + 8.6 * np.exp(-0.09 * h ^ 2) * (1 - np.exp(-dp / 50))
+    
+    def _Ground_c(h, dp):
+        return 1.5 + 14 * np.exp(-0.46 * h ^ 2) * (1 - np.exp(-dp / 50))
+    
+    def _Ground_d(h, dp):
+        return 1.5 + 5 * np.exp(-0.9 * h ^ 2) * (1 - np.exp(-dp / 50))
+    
 
 class Spectrum:
 
@@ -22,8 +106,11 @@ class Spectrum:
         self.linear = linear
 
 
-    def A():
-        pass
+    def A(self):
+        a_weighting = np.array([-26.2, -16.1, -8.6, -3.2, 0, 1.2, 1, -1.1])
+        A = self.linear + a_weighting
+        return np.log10(np.sum( np.power(A,10) ) )
+    
 
 class NoiseSource:
      
@@ -36,10 +123,10 @@ class NoiseSource:
         self.power = power
     
     def setPressure(self, pressure, dist, sphere):
-        self.power.linear = pressure.linear + 20*np.log10(dist) - np.log10(sphere)
+        self.power.linear = pressure.linear + 20*np.log10(dist) - 10*np.log10(sphere) + 11
          
-    def print(self):
-        return "location={0}, power={1}".format(self.location, self.power.linear)
+    def __str__(self):
+        return "SRC: location={0}, power={1}".format(self.location, self.power.linear)
 
 
 
@@ -48,10 +135,14 @@ class NoiseReceiver:
     
     def __init__(self,location):
         self.location = location
-        self.pressure = Spectrum
+        self.pressure = Spectrum(np.zeros([8,1]))
         
+    def __str__(self):
+        return "RCV: location={0}, pressure={1}".format(self.location, self.pressure.linear)
+
+       
     def clear(self):
-        self.pressure = Spectrum
+        self.pressure = Spectrum(np.zeros([8,1]))
     
     
     # possibly should add each contribution as a list to get partial levels
@@ -62,12 +153,7 @@ class NoiseReceiver:
         else:
             self.pressure.linear = 10*np.log10( np.power( self.pressure.linear/10, 10) \
                                                + np.power(pressure.linear/10, 10 ) )
-         
-    def print(self):
-        return "location={0}, pressure={1}".format(self.location, self.pressure.linear)
 
-        
-    
 
 class Path:
     absorption = []
@@ -82,107 +168,35 @@ if __name__ == "__main__":
 
     iso = ISO9613() # create new ISO instance
     
-    logging.debug(iso.printEnvironment())
+    logging.debug(iso)
     
     # create a source
     s_loc = Point(0,0,1.5)
-    s_spec = Spectrum(np.array([60,60,60,60,60,60,60]))
+    s_spec = Spectrum(np.array([60,60,60,60,60,60,60,60]))
     
     source = NoiseSource(location=s_loc)
-    source.setPressure(pressure=s_spec,dist=20,sphere=0.5)
-    print(source.print())
+    source.setPressure(pressure=s_spec,dist=10,sphere=0.5)
+    print(source)
+#    print("Source sound power. Should be 94 dB: {0}".format(source.power.A()))
     
-    
-    
+
     # create a receiver
     r_loc = Point(100,0,1.5)
     receiver = NoiseReceiver(r_loc)
-    
-    
-    #s = NoiseSource(point=Point(0,0,0),power=Spectrum([50 50 50 50 50 50 50 50])
-    
-    
-    
-    # create new source
-    
-    # create receiver
+    receiver.clear()
+
+    print("")
+    print("Before calc")
+    print(receiver)
+
+    iso.calc(source,None,receiver)
+
+    print("")
+    print("After calc")
+    print(receiver)
+    print(receiver.pressure.A())
         
 
 
-# Public Function ISO_GroundAbsorption(F As String, dp As Double, Gs As Double, Gm As Double, Gr As Double, hs As Double, hr As Double) As Double
-# ' Table 3
 
-# Dim q As Double
-# Dim A_s As Double, A_m As Double, A_r As Double
-
-# On Error GoTo ErrorHandler
-
-# If dp <= 30 * (hs + hr) Then
-    # q = 0
-# Else
-    # q = 1 - ((30 * (hs + hr)) / dp)
-# End If
-
-# Select Case F
-
-    # Case 63
-        # A_s = -1.5
-        # A_r = -1.5
-        # A_m = -3 * q ^ 2
-    # Case 125
-        # A_s = -1.5 + Gs * Ground_a(hs, dp)
-        # A_r = -1.5 + Gr * Ground_a(hr, dp)
-        # A_m = -3 * q * (1 - Gm)
-    # Case 250
-        # A_s = -1.5 + Gs * Ground_b(hs, dp)
-        # A_r = -1.5 + Gr * Ground_b(hr, dp)
-        # A_m = -3 * q * (1 - Gm)
-    # Case 500
-        # A_s = -1.5 + Gs * Ground_c(hs, dp)
-        # A_r = -1.5 + Gr * Ground_c(hr, dp)
-        # A_m = -3 * q * (1 - Gm)
-    # Case 1000
-        # A_s = -1.5 + Gs * Ground_d(hs, dp)
-        # A_r = -1.5 + Gr * Ground_d(hr, dp)
-        # A_m = -3 * q * (1 - Gm)
-    # Case 2000, 4000, 8000
-        # A_s = -1.5 * (1 - Gs)
-        # A_r = -1.5 * (1 - Gr)
-        # A_m = -3 * q * (1 - Gm)
-    # Case Else
-        # MsgBox (F & " is not a valid frequency")
-        # Error (1)
-        
-# End Select
-
-# ISO_GroundAbsorption = A_s + A_m + A_r
-# Exit Function
-
-# ErrorHandler:
-# MsgBox (Err.Description)
-
-# End Function
-
-# Public Function Ground_a(h, dp)
-
-    # Ground_a = 1.5 + 3 * Exp(-0.12 * (h - 5) ^ 2) * (1 - Exp(-dp / 50)) + 5.7 * Exp(-0.09 * h ^ 2) * (1 - Exp(-2.8 * 10 ^ (-6) * dp ^ 2))
-
-# End Function
-# Public Function Ground_b(h, dp)
-
-    # Ground_b = 1.5 + 8.6 * Exp(-0.09 * h ^ 2) * (1 - Exp(-dp / 50))
-
-# End Function
-
-# Public Function Ground_c(h, dp)
-
-# Ground_c = 1.5 + 14 * Exp(-0.46 * h ^ 2) * (1 - Exp(-dp / 50))
-
-# End Function
-
-# Public Function Ground_d(h, dp)
-
-# Ground_d = 1.5 + 5# * Exp(-0.9 * h ^ 2) * (1 - Exp(-dp / 50))
-
-# End Function
 
